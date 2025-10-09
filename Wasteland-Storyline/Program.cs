47a42d9942ccc7c6d9eadbfe1e6c7fcf116d7594
JamesonPhong <000882371@mohawkcollege.ca>
2025-10-08 23:34:19 -0400

    Add introduction.

---
+++ Wasteland-Storyline/Program.cs
@@ -0,0 +1,73 @@
+﻿// Introduction to the Wasteland Storyline
+Console.WriteLine("You suddenly find yourself inside some sort of buggy-like vehicle chasing after another buggy-like vehicle " +
+                  "at high speeds. The unbearable heat of the sun beats down upon you, feeling hotter than it usually does. " +
+                  "Had it not been for the bandana wrapped tightly around your face and goggles protecting your eyes, the " +
+                  "sand flying from the vehicle ahead of you would’ve blinded and choked you out.");
+
+// This block of code pauses the dialouge to give the Player time to read.
+bool pauseDialouge = true;
+while (pauseDialouge == true)
+{
+    Console.WriteLine("");
+    Console.WriteLine("Enter in the letter \"C\" To Continue...");
+    string contDialouge = Console.ReadLine();
+    if (contDialouge.ToLower() == "c")
+    {
+        pauseDialouge = false;
+    }
+}
+
+Console.WriteLine("");
+Console.WriteLine("Vernon (You): “What the fuck!? What’s going on!?”");
+Console.WriteLine("Bandit Morrison: “Hurry up V, we’ve gotta catch up to those scum-suckers before they make off with our " +
+                  "supplies!”");
+
+pauseDialouge = true;
+while (pauseDialouge == true)
+{
+    Console.WriteLine("");
+    Console.WriteLine("Enter in the letter \"C\" To Continue...");
+    string contDialouge = Console.ReadLine();
+    if (contDialouge.ToLower() == "c")
+    {
+        pauseDialouge = false;
+    }
+}
+
+Console.WriteLine("");
+Console.WriteLine("Bandit Morrison: “Gyahahaha! Light ‘em up W! Let’s teach these dust-eaters what happens to thieves who " +
+                  "steal supplies we stole first!”");
+
+pauseDialouge = true;
+while (pauseDialouge == true)
+{
+    Console.WriteLine("");
+    Console.WriteLine("Enter in the letter \"C\" To Continue...");
+    string contDialouge = Console.ReadLine();
+    if (contDialouge.ToLower() == "c")
+    {
+        pauseDialouge = false;
+    }
+}
+
+Console.WriteLine("");
+Console.WriteLine("Glancing at the rear-view mirror you see Worsley garbed in bandit clothing on top of a gatling turret. The " +
+                  "whirring of the barrel serves as a warning to the incoming storm of bullets that pelt against the fleeing " +
+                  "vehicle’s shield. Very small but noticeable cracks begin to appear in the shield. One of the bandits from " +
+                  "the vehicle gets up and lifts what appears to be a quad launcher. A red laser appears right in between you " +
+                  "and Morrison.");
+
+pauseDialouge = true;
+while (pauseDialouge == true)
+{
+    Console.WriteLine("");
+    Console.WriteLine("Enter in the letter \"C\" To Continue...");
+    string contDialouge = Console.ReadLine();
+    if (contDialouge.ToLower() == "c")
+    {
+        pauseDialouge = false;
+    }
+}
+
+Console.WriteLine("");
+Console.WriteLine("Bandit Morrison: “Shit! Dodge V! Dodge!”");
