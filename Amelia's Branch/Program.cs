45a5d81a4d331c1b59cfad01c841a8eea4ba4cc5
Amelia <amelunlun@gmail.com>
2025-10-09 15:18:35 -0400

    changed templates

---
+++ Amelia's Branch/Program.cs
@@ -0,0 +1,2 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("You decide to grab your KA-BAR combat knife in your last moments before being sucked into the ominous purple wormhole. On the other side, you appear to have been taken to some dingy and decrepit old house.");
