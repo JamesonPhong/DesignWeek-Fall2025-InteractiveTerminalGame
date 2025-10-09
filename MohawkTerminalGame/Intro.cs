d5dc621f8ebcf1b3c969469f6a747fbdf86f7a55
StitchOhana13 <Marcus13Faria@icloud.com>
2025-10-09 13:52:45 -0400

    Updated fill in combat template

---
+++ MohawkTerminalGame/Intro.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MohawkTerminalGame;
+
+namespace MohawkTerminalGame
+{
+    internal class Intro
+    {
+        public void Setup()
+        {
+            Program.TerminalExecuteMode = TerminalExecuteMode.ExecuteOnce;
+            Program.TerminalInputMode = TerminalInputMode.KeyboardReadAndReadLine;
+
+            Terminal.SetTitle("Intro");
+            Terminal.RoboTypeIntervalMilliseconds = 50; // 50 milliseconds
+            Terminal.UseRoboType = true; // use slow character typing
+            Terminal.WriteWithWordBreaks = true; // donbreak around wors, don't cut them off
+            Terminal.WordBreakCharacter = ' '; // break on spaces
+        }
+
+        public void Execute()
+        {
+            Terminal.BackgroundColor = ConsoleColor.Black;
+            Terminal.WriteLine("Press SPACE to start");
+            Terminal.WriteLine("");
+        }
+    }
+}
--- MohawkTerminalGame/TerminalGame.cs
+++ MohawkTerminalGame/TerminalGame.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Microsoft.VisualBasic.FileIO;
 using MohawkTerminalGame;
 
 namespace MohawkTerminalGame
@@ -17,27 +18,8 @@ namespace MohawkTerminalGame
             Program.TerminalInputMode = TerminalInputMode.KeyboardReadAndReadLine;
 
             //Terminal.SetCursorPosition(0, 20);
-            //Terminal.WriteLine
-            //    ("------------------------------------------------------------------------------------------------------------------------------------------------------------" +
-            //    "|                                                                                                                                                          |" +
-            //    "|                                                                                                                                                          |" +
-            //    "|                                                                                                                                                          |" +
-            //    "|                                                                                                                                                          |" +
-            //    "|                                                                                                                                                          |" +
-            //    "|                                                                                                                                                          |" +
-            //    "|                                                                                                                                                          |" +
-            //    "|                                                                                                                                                          |" +
-            //    "|                                                        hello                                                                                             |" +
-            //    "|                                                                                                                                                          |" +
-            //    "|                                                                                                                                                          |" +
-            //    "|                                                                                                                                                          |" +
-            //    "|                                                                                                                                                          |" +
-            //    "|                                                                                                                                                          |" +
-            //    "|                                                                                                                                                          |" +
-            //    "|                                                                                                                                                          |" +
-            //    "------------------------------------------------------------------------------------------------------------------------------------------------------------");
 
-            Terminal.SetTitle("Text Adventure Sample");
+            Terminal.SetTitle("Combat template");
             Terminal.RoboTypeIntervalMilliseconds = 50; // 50 milliseconds
             Terminal.UseRoboType = true; // use slow character typing
             Terminal.WriteWithWordBreaks = true; // donbreak around wors, don't cut them off
@@ -55,9 +37,9 @@ namespace MohawkTerminalGame
             Terminal.RoboTypeIntervalMilliseconds = 0;
             Terminal.Beep();
             Terminal.WriteLine("");
-            Terminal.WriteLine("Player Health");
+            Terminal.WriteLine("[PLAYERHEALTH]");
             Terminal.WriteLine(playerHealth);
-            Terminal.WriteLine("EnemyHealth");
+            Terminal.WriteLine("[ENEMYHEALTH]]");
             Terminal.WriteLine(enemyHealth);
             Terminal.Beep();
             Terminal.RoboTypeIntervalMilliseconds = 10;
@@ -67,39 +49,39 @@ namespace MohawkTerminalGame
                 if (enemyHealth > 0)
                 {
                     Terminal.WriteLine("");
-                    Terminal.WriteLine("Lorem ipsum dolor shit");
-                    Terminal.WriteLine("Choose your attack (1,2,3)");
+                    Terminal.WriteLine("[FILL IN TEXT]");
+                    Terminal.WriteLine("[ATTACK OPTIONS] [1,2,3]");
                     string answer = Terminal.ReadAndClearLine();
                     if (answer.ToLower().Equals("1"))
                     {
                         Terminal.RoboTypeIntervalMilliseconds = 20;
-                        Terminal.WriteLine("you failed, you take 20 damage.");
+                        Terminal.WriteLine("[RESPONCE TO OPTION] = You failed, you take 20 damage.");
                         playerHealth = playerHealth - 100;
                         return;
                     }
                     else if (answer.ToLower().Equals("2"))
                     {
                         Terminal.RoboTypeIntervalMilliseconds = 20;
-                        Terminal.WriteLine("You deal 20 damage");
+                        Terminal.WriteLine("[RESPONCE TO OPTION] = You deal 20 damage");
                         enemyHealth = enemyHealth - 20;
                         return;
                     }
                     else if (answer.ToLower().Equals("3"))
                     {
                         Terminal.RoboTypeIntervalMilliseconds = 20;
-                        Terminal.WriteLine("Wow impressive you deal 100 damage");
+                        Terminal.WriteLine("[RESPONCE TO OPTION] = Wow impressive you deal 100 damage");
                         enemyHealth = enemyHealth - 100;
                         return;
                     }
                 }
                 else if (enemyHealth <= 0)
                 {
-                    Terminal.WriteLine("You did it!");
+                    Terminal.WriteLine("[RESPONCE TO VICTORY] = You did it!");
                 }
             }
             else if (playerHealth <= 0)
             {
-                Terminal.WriteLine("What are you? An idiot sandwich!!!!");
+                Terminal.WriteLine("[RESPONCE TO FAILURE] = What are you? An idiot sandwich!!!!");
             }
 
             string end = Terminal.ReadAndClearLine();
